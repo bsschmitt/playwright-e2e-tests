487ffa89c6884d8402ca191aaf9d813e43e9f6e3
Bruno Stephan Schmitt <brunosschmitt@gmail.com>
2026-04-06 15:18:50 -0300

    Teste simples login com page

---
+++ Pages/LoginPage.ts
@@ -0,0 +1,31 @@
+import {Page} from '@playwright/test';
+
+export class LoginPage{
+    readonly page: Page;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    async acessarSite(){
+        await this.page.goto('https://www.saucedemo.com/');
+    }
+
+    async preencherUsuario(usuario: string) {
+        await this.page.locator('#user-name').fill(usuario);
+    }
+
+    async preencherSenha(senha: string) {
+        await this.page.locator('#password').fill(senha);
+    }
+
+    async clicarLogin() {
+        await this.page.locator('#login-button').click();
+    }
+
+    async login(usuario: string, senha: string){
+        await this.preencherUsuario(usuario);
+        await this.preencherSenha(senha);
+        await this.clicarLogin();
+    }
+}

f6a84554575f2db752447f2127a578d056a0887d
Bruno Stephan Schmitt <brunosschmitt@gmail.com>
2026-04-16 14:50:16 -0300

    Test: implement cart feature tests with item validation and removal

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -1,25 +1,46 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { LoginPage } from '../Pages/LoginPage';
 import { InventoryPage } from '../Pages/InventoryPage';
 import { CartPage } from '../Pages/CartPage';
 
 test.describe('Testes carrinho', () => {
 
-    test('Adicionar e remover item do carrinho', async ({ page }) => {
+    test.beforeEach(async ({page}) => {
         const login = new LoginPage(page);
+        await login.acessarSite();
+        await login.login('standard_user', 'secret_sauce');
+    });
+
+    test('CT01 - Carrinho inicia vazio', async ({ page }) => {
+        const inventory = new InventoryPage(page);
+        await inventory.abrirCarrinho();
+        await expect(page.locator('.cart_item')).toHaveCount(0);
+    });
+
+    test('CT02 - Adicionar item e validar no carrinho', async ({page}) => {
         const inventory = new InventoryPage(page);
         const cart = new CartPage(page);
 
-        //Login
-        await login.acessarSite();
-        await login.login('standard_user', 'secret_sauce');
-
-        //Operações com o carrinho
         await inventory.adicionarProdutoCarrinho();
         await inventory.abrirCarrinho();
-
         await cart.validaItemCarrinho();
+    });
+
+    test('CT03 - Remover item do carrinho', async ({page}) => {
+        const inventory = new InventoryPage(page);
+        const cart = new CartPage(page);
+        //Adicionando item no carrinho
+        await inventory.adicionarProdutoCarrinho();
+        await inventory.abrirCarrinho();
+        
+        //Validando item adicionado
+        await cart.validaItemCarrinho()
+        
+        //Removendo item do carrinho
         await cart.removerItem();
+
+        //Vaçidando carrinho vazio
         await cart.validaCarrinhoVazio();
     });
 });
+

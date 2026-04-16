7ab3ca3895ee4df5eef1aa8035dd5685081fa711
Bruno Stephan Schmitt <brunosschmitt@gmail.com>
2026-04-16 15:22:26 -0300

    Test: Implement checkout main flow with form validation

--- tests/fluxoPrincipal.spec.ts
+++ tests/fluxoPrincipal.spec.ts
@@ -1,33 +1,50 @@
-import { test } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 import { LoginPage } from '../Pages/LoginPage'
 import { InventoryPage } from '../Pages/InventoryPage'
 import { CartPage } from '../Pages/CartPage'
 import { CheckoutPage } from '../Pages/CheckoutPage'
 
 test.describe('E2E', () => {
-
-    test('Fluxo completo de compra', async({ page }) => {
+    test.beforeEach(async ({page}) => {
         const login = new LoginPage(page);
         const inventory = new InventoryPage(page);
-        const cart = new CartPage(page);
-        const checkout = new CheckoutPage(page);
 
         //Login
         await login.acessarSite();
         await login.login('standard_user', 'secret_sauce');
 
-        //Adicionando item carrinho
+        //Adicionando item ao carrinho
         await inventory.adicionarProdutoCarrinho();
         await inventory.abrirCarrinho();
+    });
 
-        //Finalizando 'compra'
+    test('CT01 - Fluxo principal de compra', async({ page }) => {
+       const cart = new CartPage(page);
+       const checkout = new CheckoutPage(page);
+
+       await cart.checkout();
+       await checkout.preencherDados();
+       await checkout.validaSucesso();
+    });
+
+    test('CT02 - Checkout sem preencher os dados', async({page}) => {
+        const cart = new CartPage(page);
+
         await cart.checkout();
-        await checkout.preencherDados();
+        await page.click('[data-test="continue"]')
 
-        //Validação
-        await checkout.validaSucesso();
+        //Validação de erro
+        await expect(page.locator('[data-test="error"]')).toContainText("First Name is required");
+    });
 
-        //Logout
-        await inventory.efetuarLogout();
-    })
-})
+    test('CT03 - Erro sem sobrenome', async ({page}) => {
+        const cart = new CartPage(page);
+
+        await cart.checkout();
+        await page.fill('[data-test="firstName"]', 'Testador');
+        await page.click('[data-test="continue"]')
+
+        //Validação de erro
+        await expect(page.locator('[data-test="error"]')).toContainText('Last Name is required');
+    });
+});
